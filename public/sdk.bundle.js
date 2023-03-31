bc57f1b417ed20143ee58ce426ef84be2d73190f
830008 <nrollins189@gmail.com>
2023-03-31 03:12:31 +0000

    GITIGNORE

--- public/sdk.bundle.js
+++ public/sdk.bundle.js
@@ -1,19 +1,6 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ([
-/* 0 */
-/***/ (() => {
-
-throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: Could not load TypeScript. Try installing with `yarn add typescript` or `npm install typescript`. If TypeScript is installed globally, try using `yarn link typescript` or `npm link typescript`.\n    at Object.loader (/home/greenworld/XenOS/node_modules/ts-loader/dist/index.js:17:18)");
-
-/***/ })
-/******/ 	]);
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__[0]();
 /******/ 	
 /******/ })()
 ;
